c3d028d0e3f67a299cee99a13a89ae3e80b29232
Richard Johansen <richard.johansen@uc.edu>
2019-02-26 13:05:35 -0500

    Updated plot

--- BabyNames/babynames_package_example.R
+++ BabyNames/babynames_package_example.R
@@ -10,7 +10,10 @@ names_year <- names %>%
   summarise(count = sum(n))
 
 ggplot(names_year, aes(x = year, y = count, col =name)) +
+  labs(x = "Year", y = "Babies Born", title = "My First and Middle Name's Popularity Trend") +
   geom_line( size=1)+
-  geom_text(aes(label = "I was \n Born", x = 1989, y = 25000), col= "black") +
+  geom_text(aes(label = "I was \nborn here", x = 1989, y = 25000), col= "black") +
+  geom_point(aes(x =1989, y = 15000), col = 'black', size  =3) + 
   theme_bw() +
-  theme(axis.text.x=element_text(angle=90))
+  theme(axis.text.x=element_text(angle=0),
+        axis.title.y = element_text(angle=0, vjust = 0.5))
