0bf993f373d008aea62d4d5b853185aa49089329
Richard Johansen <richard.johansen@uc.edu>
2019-03-11 16:32:10 -0400

    Updated color palette

--- DataVis_Society/dvs_members.R
+++ DataVis_Society/dvs_members.R
@@ -1,12 +1,17 @@
+library(maps)
 library(tidyverse)
 df <- read.csv("C:/R_Packages/DataVizFriday/DataVis_Society/dvs_challenge.csv")
-
-df_sub <- df[1:50,]
 
 df$red <- (df$data-min(df$data))/(max(df$data)-min(df$data))
 df$green <- (df$visualization-min(df$visualization))/(max(df$visualization)-min(df$visualization))
 df$blue <- (df$society-min(df$society))/(max(df$society)-min(df$society))
 
-ggplot(df_sub,aes(long,lat)) +
-  geom_point(aes(col = rgb(r=var1, g=var2, b=var3))) +
+df_sub <- df[1:50,]
+
+mapWorld <- borders("world", colour="gray50", fill="gainsboro", ylim=c(-60, 90))
+
+ggplot() +
+  mapWorld +
+  geom_point(aes(df$long,df$lat,col = rgb(r=df$red, g=df$green, b=df$blue))) +
+  scale_color_identity() +
   theme_classic()
